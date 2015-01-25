a6a6bb466ebfd3d026899974326379d32f297226
Ayesha Ilyas <ayesha.nizami@gmail.com>
2015-01-25 04:11:13 -0800

    Formatting

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -28,10 +28,10 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('are URL defined and not empty',function(){
-            for (entry in allFeeds){
+         it('are URL defined and not empty',function() {
+            for (entry in allFeeds) {
                 expect(allFeeds[entry].url).toBeDefined();
-                expect(allFeeds[entry].url.length).not.toBe(0);   
+                expect(allFeeds[entry].url.length).not.toBe(0);
             }
             
          });
@@ -42,10 +42,10 @@ $(function() {
          * and that the name is not empty.
          */
 
-         it('are names defined and not empty',function(){
-            for (entry in allFeeds){
+         it('are names defined and not empty',function() {
+            for (entry in allFeeds) {
                 expect(allFeeds[entry].name).toBeDefined();
-                expect(allFeeds[entry].name.length).not.toBe(0);   
+                expect(allFeeds[entry].name.length).not.toBe(0);
             }
             
          });
@@ -55,13 +55,13 @@ $(function() {
     *   The Menu test suite to ensure correct behaviour of the menu. Hides/Dsiplays approprietly.
     */
         
-    describe('The menu', function(){
+    describe('The menu', function() {
 
         /* Test that ensures the menu element is
          * hidden by default. Check if it has the class menu-hidden. If so, it will be invisible. 
          */
 
-        it('is hidden by default',function(){
+        it('is hidden by default',function() {
             expect($('body').hasClass('menu-hidden')).toEqual(true);
         });
          
@@ -73,28 +73,27 @@ $(function() {
           */
         var menuButton;
        
-        it('does change visibility',function(){
+        it('does change visibility',function() {
             menuButton=$('.menu-icon-link');
             /**
             *  Trigger menu click event to show menu,
             *  body should not have the menu-hidden class
             */
-            menuButton.click(); 
-            expect($('body').hasClass('menu-hidden')).toEqual(false);   
+            menuButton.click();
+            expect($('body').hasClass('menu-hidden')).toEqual(false);
 
             /**
             *  Trigger menu click event again to hide menu,
             *  body should have the menu-hidden class
             */
             menuButton.click();
-            expect($('body').hasClass('menu-hidden')).toEqual(true);                
+            expect($('body').hasClass('menu-hidden')).toEqual(true);
 
-        });  
+        });
 
     });
     /* Initial Entries tes suite */
-    describe('Initial Enteries',function(){
-        
+    describe('Initial Enteries',function() {
         /* 
         * Test that ensures when the loadFeed function is called and completes its work, there is at least
         * a single .entry element within the .feed container.
@@ -103,23 +102,22 @@ $(function() {
         /*
         * Before the test, load the feed with loadFeed(...)
         * Pass done() as the callback function for load, it will be called when loadFeed() finishes running, asynchronoulsy */
-        beforeEach(function(done){
+        beforeEach(function(done) {
             google.load('feeds', '1');
             loadFeed(0,done);
         });
         /*
         * Class .feed contains all the entries in a feed inside the .entry class. Check to see if it's length is greater than 0
         */
-        it('has been loaded',function(){
+        it('has been loaded',function() {
             expect($('.feed').has('.entry').length).toBeGreaterThan(0);
         });
 
     });
 
     /* "New Feed Selection: Test Suite"*/
-
         
-    describe('New Feed Selection', function(){
+    describe('New Feed Selection', function() {
         
         /* Test that ensures when a new feed is loaded by the loadFeed function that the content actually changes.
          * Compare the current values of the headings (titles in a particular new feed) with the new values of heading in the feed
@@ -127,18 +125,19 @@ $(function() {
          */
 
         var headingsBefore;
-        beforeEach(function(done){     
+        beforeEach(function(done) {
             headingsBefore=$('.entry-link').find('h2');
             /* Load a new feed*/
             loadFeed(1,done);
 
          });
-        it ('has new feed loaded',function(){
+        
+        it ('has new feed loaded',function() {
            var headingsAfter=$('.entry-link').find('h2');
-           expect(headingsBefore).not.toEqual(headingsAfter);     
-
+           expect(headingsBefore).not.toEqual(headingsAfter);
         });
-        afterEach(function(done){
+
+        afterEach(function(done) {
             /*load back old feed*/
             loadFeed(0,done);
 
